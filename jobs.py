e50c2f114afc30e4067c01f83d7a058e5e91ab3a
giorgostsilivis <tsilivisgeo@gmail.com>
2022-01-31 00:48:46 +0200

    jobfix

--- jobs.py
+++ jobs.py
@@ -33,18 +33,27 @@ def job():
     input2.to_sql('vessels_static', cnx,if_exists='replace',index=False)
     input3.to_sql('port_events', cnx,if_exists='replace',index=False)
     view = '''
-    WITH temporaryTable (ship_id,port_name,event_type,event_timestamp) as
-    (select ship_id,port_name,event_type,max(event_timestamp) from port_events group by ship_id)
-
-    SELECT vessels_dynamic.ship_id,vessels_dynamic.lon,
-    vessels_dynamic.lat,vessels_dynamic.course,
-    vessels_dynamic.speed,vessels_dynamic."timestamp",
-    vessels_static.shipname,vessels_static."length",
-    vessels_static.width, temporaryTable.port_name, temporaryTable.event_type,
-    temporaryTable.event_timestamp
-    FROM ((vessels_dynamic
-    INNER JOIN vessels_static ON vessels_dynamic.ship_id = vessels_static.ship_id)
-    LEFT JOIN temporaryTable ON vessels_static.ship_id = temporaryTable.ship_id);
+    with dynastatic (ship_id,lon,lat,course,speed,timestamp,shipname,length,width) AS
+    (select vessels_dynamic.ship_id,vessels_dynamic.lon,
+        vessels_dynamic.lat,vessels_dynamic.course,
+        vessels_dynamic.speed,vessels_dynamic."timestamp",
+        vessels_static.shipname,vessels_static."length",
+        vessels_static.width from vessels_dynamic inner join vessels_static on vessels_dynamic.ship_id = vessels_static.ship_id),
+    
+    final (ship_id,lon,lat,course,speed,timestamp,shipname,length,width,port_name,event_type,event_timestamp) as
+    (select dynastatic.ship_id,dynastatic.lon,
+        dynastatic.lat,dynastatic.course,
+        dynastatic.speed,dynastatic."timestamp",
+        dynastatic.shipname,dynastatic."length",
+        dynastatic.width,port_events.port_name,port_events.event_type,port_events.event_timestamp from dynastatic left join port_events on dynastatic.ship_id = port_events.ship_id),
+    
+    myview as (select ship_id,lon,lat,course,speed,timestamp,shipname,length,width,
+    case when timestamp<event_timestamp then null else event_timestamp end as event_timestamp,
+    case when timestamp<event_timestamp then null else port_name end as port_name,
+    case when timestamp<event_timestamp then null else event_type end as event_type
+    from final)
+    
+    select ship_id,lon,lat,course,speed,timestamp,shipname,length,width,max(port_name) as port_name,max(event_type) as event_type,max(event_timestamp) as event_timestamp from myview group by ship_id,lon,lat,course,speed,timestamp,shipname,length,width order by timestamp desc
     '''
     output_data = pd.read_sql_query(view, cnx)
     # print(input_data)
